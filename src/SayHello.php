0622241ba2e66b5583c5fc152314b8ff8fdb05d4
chenyanjia <chenyanjia@mbcore.com>
2017-12-07 20:24:24 +0800

    demo提交

---
+++ src/SayHello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/7
+ * Time: 20:07
+ */
+namespace MBCore;
+
+class SayHello
+{
+    public static function Say()
+    {
+        return 'Hello MBCore!';
+    }
+}
---
+++ test/SayHello.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/7
+ * Time: 20:16
+ */
+require dirname(__DIR__) . '/vendor/autoload.php';
+use MBCore\SayHello;
+echo SayHello::Say();
